bc5715d7e24dac7ed451ab6332e4213fcbbb3831
admclamb <adylanmclamb@yahoo.com>
2022-01-21 23:04:22 -0500

    init main

---
+++ javaScript-drumKit/src/main.js
@@ -0,0 +1,10 @@
+const keysNode = document.querySelectorAll(".key");
+
+keysNode.forEach((element) => {
+  console.log(element);
+});
+
+
+document.addEventListener("keydown", (event) => {
+  if ()
+})
