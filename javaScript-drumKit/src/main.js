9b2c1bd266b0f784de94be0570f056c231a59039
admclamb <adylanmclamb@yahoo.com>
2022-01-22 09:56:02 -0500

    finished project

--- javaScript-drumKit/src/main.js
+++ javaScript-drumKit/src/main.js
@@ -1,10 +1,42 @@
 const keysNode = document.querySelectorAll(".key");
 
+const keyToSound = {
+  65: "clap",
+  83: "hihat",
+  68: "kick",
+  70: "openhat",
+  71: "boom",
+  72: "ride",
+  74: "snare",
+  75: "tom",
+  76: "tink",
+};
+
+const playSound = (key) => {
+  let audio = new Audio(`sounds/${keyToSound[key]}.wav`);
+  audio.play();
+};
+
+const toggleKeyHitClass = (element) => {
+  element.classList.add("key-hit");
+  setTimeout(() => {
+    element.classList.remove("key-hit");
+  }, 90);
+};
+
 keysNode.forEach((element) => {
-  console.log(element);
+  element.addEventListener("click", () => {
+    toggleKeyHitClass(element);
+    let key = element.dataset.key;
+    playSound(key);
+  });
 });
 
-
+let keys = [65, 83, 68, 70, 71, 72, 74, 75, 76];
 document.addEventListener("keydown", (event) => {
-  if ()
-})
+  let key = event.keyCode;
+  if (!keys.includes(key)) return;
+  const keyElement = document.querySelector(`[data-key='${key}'`);
+  toggleKeyHitClass(keyElement);
+  playSound(key);
+});
