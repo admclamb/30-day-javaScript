a3865816ad3bb74879ed4f18837a01291adfdb15
admclamb <adylanmclamb@yahoo.com>
2022-02-07 21:45:29 -0500

    commit on main

---
+++ 03-Playing-with-CSS-variables/main.js
@@ -0,0 +1,4 @@
+"use strict";
+const spacingInput = document.querySelector("#spacing");
+const blurInput = document.querySelector("#blur");
+spacingInput.addEventListener("click", () => console.log("onClicked!"));
